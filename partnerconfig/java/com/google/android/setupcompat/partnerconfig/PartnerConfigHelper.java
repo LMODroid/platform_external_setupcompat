0e5714f643d7f6e7c85cb1f876309e0b5e5a35af
Chirayu Desai <chirayudesai1@gmail.com>
2023-11-03 02:29:34 +0530

    Silence log spam
    Change spammy log messages to be verbose messages that are not logged
    by default. They can be logged again with:
    `adb shell setprop persist.log.tag.PartnerConfigHelper VERBOSE`.
    
    Change-Id: Ib95b8056dbf07c45c236fdce37b0a5dd04410f96

--- partnerconfig/java/com/google/android/setupcompat/partnerconfig/PartnerConfigHelper.java
+++ partnerconfig/java/com/google/android/setupcompat/partnerconfig/PartnerConfigHelper.java
@@ -53,6 +53,8 @@ public class PartnerConfigHelper {
 
   private static final String TAG = PartnerConfigHelper.class.getSimpleName();
 
+  private static boolean LOGV = Log.isLoggable(TAG, Log.VERBOSE);
+
   public static final String SUW_AUTHORITY = "com.google.android.setupwizard.partner";
 
   @VisibleForTesting public static final String SUW_GET_PARTNER_CONFIG_METHOD = "getOverlayConfig";
@@ -953,7 +955,7 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "SetupWizard DayNight supporting status unknown; return as true.");
+        logv("SetupWizard DayNight supporting status unknown; return as true.");
         suwDayNightEnabledBundle = null;
         return true;
       }
@@ -976,9 +978,7 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(
-            TAG,
-            "SetupWizard extended partner configs supporting status unknown; return as true.");
+        logv("SetupWizard extended partner configs supporting status unknown; return as true.");
         applyExtendedPartnerConfigBundle = null;
         return true;
       }
@@ -1012,7 +1012,7 @@ public class PartnerConfigHelper {
           return shouldApplyExtendedPartnerConfig(context);
         }
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "SetupWizard Material You configs supporting status unknown; return as false.");
+        logv("SetupWizard Material You configs supporting status unknown; return as false.");
         applyMaterialYouConfigBundle = null;
         return false;
       }
@@ -1040,7 +1040,7 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "SetupWizard default theme status unknown; return as null.");
+        logv("SetupWizard default theme status unknown; return as null.");
         suwDefaultThemeBundle = null;
         return null;
       }
@@ -1064,7 +1064,7 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "SetupWizard dynamic color supporting status unknown; return as true.");
+        logv("SetupWizard dynamic color supporting status unknown; return as true.");
         applyDynamicColorBundle = null;
         return true;
       }
@@ -1087,7 +1087,7 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "SetupWizard full dynamic color supporting status unknown; return as false.");
+        logv("SetupWizard full dynamic color supporting status unknown; return as false.");
         applyFullDynamicColorBundle = null;
         return false;
       }
@@ -1110,9 +1110,7 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(
-            TAG,
-            "SetupWizard one-pane support in embedded activity status unknown; return as false.");
+        logv("SetupWizard one-pane support in embedded activity status unknown; return as false.");
         applyEmbeddedActivityOnePaneBundle = null;
         return false;
       }
@@ -1136,7 +1134,7 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(TAG, "Neutral button style supporting status unknown; return as false.");
+        logv("Neutral button style supporting status unknown; return as false.");
         applyNeutralButtonStyleBundle = null;
         return false;
       }
@@ -1185,9 +1183,7 @@ public class PartnerConfigHelper {
                     /* arg= */ null,
                     /* extras= */ null);
       } catch (IllegalArgumentException | SecurityException exception) {
-        Log.w(
-            TAG,
-            "applyGlifThemeControlledTransition unknown; return applyGlifThemeControlledTransition"
+        logv("applyGlifThemeControlledTransition unknown; return applyGlifThemeControlledTransition"
                 + " as default value");
       }
     }
@@ -1333,6 +1329,12 @@ public class PartnerConfigHelper {
       contentObserver = null;
     } catch (SecurityException | NullPointerException | IllegalArgumentException e) {
       Log.w(TAG, "Failed to unregister content observer: " + e);
+    }
+  }
+
+  private static void logv(String msg) {
+    if (LOGV) {
+      Log.v(TAG, msg);
     }
   }
 }
